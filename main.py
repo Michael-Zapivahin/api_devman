d7df4c1de1311a8b966c4f5d7ec91ceaec456947
Michael-Zapivahin <zapivahin@ya.ru>
2023-06-08 16:56:29 +0300

    fixed message processing

--- main.py
+++ main.py
@@ -6,65 +6,52 @@ import telebot
 
 from datetime import datetime
 from dotenv import load_dotenv
+from requests.exceptions import HTTPError, ConnectionError
 
 
 def send_message(bot_token, chat_id, message):
-    bot = telebot.TeleBot(bot_token)
-    try:
-        if not message['is_negative']:
-            message_text = 'Your work completed successfully.'
-        else:
-            message_text = 'The tutor returned your work for revision.'
-        message_text += f' link to work {message["lesson_url"]}'
-    except KeyError as error:
-        message_text = f'KeyError {error}'
-        bot.send_message(chat_id, message_text)
-        message_text = f'message {message}'
-        bot.send_message(chat_id, message_text)
 
+    if not message['status'] == 'found':
+        return
+
+    lesson_url = ''
+    message_text = 'The tutor returned your work for revision.'
+    for attempt in message['new_attempts']:
+        lesson_url = attempt['lesson_url']
+        if not attempt['is_negative']:
+            message_text = 'Your work completed successfully.'
+            break
+
+    message_text += f'  {lesson_url}'
+    bot = telebot.TeleBot(bot_token)
     bot.send_message(chat_id, message_text)
 
 
 def long_polling(devman_token, bot_token, chat_id):
 
-    timestamp = datetime.timestamp(datetime.now())
-    last_attempt_timestamp = f'{int(timestamp)}'
-
+    timestamp_to_request = int(datetime.timestamp(datetime.now()))
     url = 'https://dvmn.org/api/long_polling/'
+
     headers = {
         'Authorization': devman_token,
     }
     while True:
         payload = {
-            'last_attempt_timestamp': last_attempt_timestamp,
+            'timestamp_to_request': timestamp_to_request,
         }
         try:
             response = requests.get(url, headers=headers, timeout=60, params=payload)
+            response.raise_for_status()
         except requests.exceptions.ReadTimeout:
             continue
-        except requests.exceptions.HTTPError as http_error:
-            print(http_error)
-            time.sleep(10)
-            continue
-        except requests.exceptions.ConnectionError as connection_error:
-            print(connection_error)
+        except HTTPError or ConnectionError as error:
+            print(error)
             time.sleep(10)
             continue
 
-        response.raise_for_status()
         works_status = response.json()
-        last_attempt_timestamp = works_status['last_attempt_timestamp']
+        timestamp_to_request = works_status['last_attempt_timestamp']
         send_message(bot_token, chat_id, works_status)
-
-
-def my_works(dvmn_token):
-    url = 'https://dvmn.org/api/user_reviews/'
-    headers = {
-        'Authorization': dvmn_token,
-    }
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    return response.json()
 
 
 def main():
@@ -72,7 +59,6 @@ def main():
     dvmn_token = os.environ['DVMN_TOKEN']
     bot_token = os.environ['TG_BOT_TOKEN']
     chat_id = os.environ['TG_CHAT_ID']
-    send_message(bot_token, chat_id, my_works(dvmn_token))
     long_polling(dvmn_token, bot_token, chat_id)
 
 
